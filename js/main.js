3df34cc45bde9f8dc65be1877f72c9c92fe1c711
SamuelGoulart <62961331+SamuelGoulart@users.noreply.github.com>
2021-07-19 22:16:24 -0300

    Colocação de parte da aminação.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,58 @@
 'use strict'
 
-const animeNav = () =>{
+const animeNav = () => {
     const windowTop = window.pageYOffset;
-    if (windowTop > 700) { document.querySelector('.navInternalLinks').classList.add('navActivate') }
-    if (windowTop < 690) { document.querySelector('.navInternalLinks').classList.remove('navActivate') }
+    if (windowTop > 500) { document.querySelector('.navInternalLinks').classList.add('navActivate') }
+    if (windowTop < 490) { document.querySelector('.navInternalLinks').classList.remove('navActivate') }
+}
+
+const underlineNav = () => {
+    const windowTop = window.pageYOffset;
+    const a = document.querySelectorAll('.navInternalLinks > ul > a')
+    if (windowTop < 700) {
+        a[3].classList.remove('activateAdherentIndex')
+    }
+
+    if (windowTop > 700) {
+        a[3].classList.add('activateAdherentIndex')
+        a[4].classList.remove('activateAdherentIndex')
+        a[5].classList.remove('activateAdherentIndex')
+    }
+
+    if (windowTop > 1300) {
+        a[3].classList.remove('activateAdherentIndex')
+        a[4].classList.add('activateAdherentIndex')
+    }
+
+    if (windowTop > 2000) {
+        a[4].classList.remove('activateAdherentIndex')
+        a[6].classList.remove('activateAdherentIndex')
+        a[5].classList.add('activateAdherentIndex')
+    }
+
+    if (windowTop > 2500) {
+        a[5].classList.remove('activateAdherentIndex')
+        a[6].classList.add('activateAdherentIndex')
+
+    }
 }
 
 
-window.addEventListener('scroll', animeNav)
+const target = document.querySelectorAll('[data-anime]')
+
+const animeScroll = () => {
+    const windowTop = window.pageYOffset + window.innerHeight * 0.75
+    target.forEach((element) => {
+        if (windowTop > element.offsetTop) {
+            element.classList.add('animate')
+        }else{
+            element.classList.remove('animate')
+        }
+    })
+
+}
+
+window.addEventListener('scroll', () => { animeNav(); underlineNav() })
+if (target.length) { window.addEventListener('scroll', animeScroll) }
+
+animeScroll()
