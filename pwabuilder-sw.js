5bbf0a4e3734feffef2d903ac8c10b5dd9f870e0
SamuelGoulart <62961331+SamuelGoulart@users.noreply.github.com>
2021-09-01 18:24:23 -0300

    Update

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,3 +1,5 @@
+// This is the "Offline copy of assets" service worker
+
 const CACHE = "pwabuilder-offline";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
