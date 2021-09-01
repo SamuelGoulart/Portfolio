54e6956542da8869b163fe9d160829ace09f3ba4
SamuelGoulart <62961331+SamuelGoulart@users.noreply.github.com>
2021-09-01 16:26:16 -0300

    update

---
+++ pwabuilder-sw/pwabuilder-sw.js
@@ -0,0 +1,18 @@
+// Este é o service worker "Cópia offline de ativos"
+
+const CACHE = "pwabuilder-offline";
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
